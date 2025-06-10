fa622a1cfa37872cd2a1fc6f117758d222a50963
arminas-bartkus <arminas.bartkus@gmail.com>
2025-06-10 18:50:16 +0100

    Add catches for common bugs
    The calculator now can take multiple operators.
    For example, if 12 + 3 is entered and then the
    - button is clicked. The display will show 15.
    Then the user can click a new set of numbers,
    i.e. 29 and the equals button and the calculator
    will calculate and display the answer to 15-29.
    This feature is made in a way so that it can loop
    infinitely. So therefore, the user can input: an
    operation like 12+4, followed by subsequent operators
    and numbers infinitely until equals is pressed.
    
    The calculator can now longer take in subsequent
    operators. like 12 + +, if a second operator is
    detected before a new number, nothing will happen.
    
    Now the calculator can easily begin a new
    calculation by pressing a new number after an
    expression is evaluated.
    
    There is a bug where if an operator is used after
    an expression is evaluated with equals, the
    calculator breaks

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Calculation logic
+
 const addition = (a, b) => a + b;
 const subtraction = (a, b) => a - b;
 const multiplication = (a, b) => a * b;
@@ -8,7 +10,14 @@ let number1 = "";
 let number2 = "";
 let operator = "";
 let numberToDisplay = "";
-let operatorUsed = false;
+
+let lastInputIsOperator = false;
+let operatorButtonUsedNow = false;
+
+let additionalOperatorUsed = false;
+
+let theoreticalNumber1 = "";
+
 
 
 let display = document.querySelector(".display");
@@ -21,12 +30,26 @@ wrapper.addEventListener('click', (event) => {
     return;
   }
 
+// ON BUTTON CLICK
+
+if  (event.target.id == "+" 
+|| event.target.id == "-"
+|| event.target.id == "/"
+|| event.target.id == "*") {
+    operatorButtonUsedNow = true;
+}
+else {
+    operatorButtonUsedNow = false;
+}
 
 
 
-if (((event.target.id == "=" ) && (!operatorUsed || numberToDisplay == ""))) {
+// If equals pressed early do nothing
+if (((event.target.id == "=" ) && (numberToDisplay == ""))) {
 
 }
+
+// if AC clicked reset calculator
 
 else if (event.target.id == "ac") {
 
@@ -34,55 +57,86 @@ else if (event.target.id == "ac") {
     number2 = "";
     operator = "";
     numberToDisplay = "";
-    operatorUsed = false;
+
+    lastInputIsOperator = false;
+    operatorButtonUsedNow = false;
+
     display.textContent = "Display";
 }
 
-else {
+// If equals pressed when calculator is ready for calc
 
-if (event.target.id == "=" &&
-    operatorUsed && number1 != "") {
+else if (event.target.id == "=" &&
+     number1 != "") {
         number2 = numberToDisplay;
         numberToDisplay = "";
-
-        // if divide by 0 snarky comment
 
         if (number2 == "0" && operator == "/") {
             display.textContent = "I won't do it!"
         }
         else {
             numberToDisplay = operate(number1, number2, operator);
+            display.textContent = numberToDisplay.toFixed(5);
+            
+            // Reset Calc apart from display
+
+            numberToDisplay = "";
+            number1 = "";
+            number2 = "";
+            operator = "";
+
+            lastInputIsOperator = false;
+            operatorButtonUsedNow = false;
+            
         }
-        display.textContent = numberToDisplay.toFixed(5);
-        numberToDisplay = "";
     }
 
-else if (operatorUsed) {
+else if (number2 != "" && operatorButtonUsedNow) {
+    
+    
+    theoreticalNumber1 = operate(number1, number2, operator)
+    operator = event.target.id;
+    number1 = theoreticalNumber1;
+    number2 = "";
+    numberToDisplay = "";
+    display.textContent = theoreticalNumber1;
+}
+
+// Update display after operator (second number) and get ready to store number to display to number2
+
+else if (lastInputIsOperator && operatorButtonUsedNow == false) {
+
     numberToDisplay += event.target.id;
+    number2 = numberToDisplay;
+
+
+
     display.textContent = numberToDisplay;
 } 
 
 
-if (numberToDisplay != "" && (
-   event.target.id == "+" 
-|| event.target.id == "-"
-|| event.target.id == "/"
-|| event.target.id == "*") &&
-   operatorUsed == false) {
+//Display operator when pressed operator button pressed after entering number 1
 
-    operatorUsed = true;
+else if (numberToDisplay != "" && operatorButtonUsedNow 
+     && lastInputIsOperator == false) {
+
+    lastInputIsOperator = true
     operator = event.target.id;
     number1 = numberToDisplay;
     numberToDisplay = "";
     display.textContent = event.target.id;
     
 }
-else if (operator == "") {
+
+// logic to update number if operator not yet pressed (i.e. inputting number1)
+else if (operatorButtonUsedNow == false) {
     numberToDisplay += event.target.id;
-display.textContent = numberToDisplay;
+    display.textContent = numberToDisplay;
 }
-}
+
 })
+
+// Calculation
 
 function operate(number1, number2, operator) {
 
